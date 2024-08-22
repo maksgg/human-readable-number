1864746ddb6408b5eea014011d7c10bff1366c49
maksgg <maks.zhovmir.97@gmail.com>
2024-08-22 18:18:14 +0300

    feat: finish task

--- src/index.js
+++ src/index.js
@@ -4,9 +4,13 @@ module.exports = function toReadable (number) {
         'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'
         ]
       const second = [
-       '', '', 'twenty', 'third', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety',
+       '', '', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety',
       ]
       if(number < 20) {
         return first[number];
-      } 
+      } else if(number < 100) {
+        return second[Math.floor(number / 10)] + (number % 10 !== 0 ? ' ' + first[number % 10] : '');
+      } else if(number < 1000) {
+        return first[Math.floor(number / 100)] + ' hundred' + (number % 100 !== 0 ? ' ' + toReadable(number % 100) : '');
+      }
 }
